7ba7040d144f44bf16574b269920ccc88a0ce8e7
Wendy Pedrosa <105825286+wxpdr@users.noreply.github.com>
2025-05-02 14:22:02 -0300

    Inicio do projeto
    Subindo apenas o projeto criado com o SpringBoot para iniciarmos

---
+++ clínica-mackito/src/main/java/saude/clinica_mackito/ClinicaMackitoApplication.java
@@ -0,0 +1,13 @@
+package saude.clinica_mackito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMackitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMackitoApplication.class, args);
+	}
+
+}
---
+++ clínica-mackito/src/test/java/saude/clinica_mackito/ClinicaMackitoApplicationTests.java
@@ -0,0 +1,13 @@
+package saude.clinica_mackito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMackitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
